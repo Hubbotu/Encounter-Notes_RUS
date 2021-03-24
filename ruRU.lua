3a9c80143f581f80dfae8b0f0a10a66051523787
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-03-24 07:41:35 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,8 @@
+L.option_anchor = "Привязать"
+L.option_anchor_desc = "К какому углу экрана привязать текстовое поле?"
+L.option_anchorX = "Смещение X"
+L.option_anchorY = "Смещение Y"
+L.option_style = "Стиль рамки"
+L.option_width = "Ширина"
+
+L.option_toggleTest = "Показать/Скрыть"
